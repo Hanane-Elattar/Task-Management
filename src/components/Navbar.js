0c6ebaee23f6d80df1777defeecca8d07b2784f4
Hanane-hue <hananeelattar405@gmail.com>
2025-01-05 17:54:55 +0100

    modified Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,32 +21,6 @@ export default function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#tasks">
-                Tasks
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
     </nav>
   );
